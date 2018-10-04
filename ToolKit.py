7fef54039e9dba48385e363e17b601cc7bc4397c
Ryan <ryan.n.foote@gmail.com>
2018-10-04 00:44:49 -0500

    fixed main landing page

--- ToolKit.py
+++ ToolKit.py
@@ -1,17 +1,10 @@
 # Area to import? Or do I want to import separate python files?
 
 # Prints main menu
-print("Welcome to OSINT Toolkit!\n----------------------------------------")
-print("Select the tool you wish to use...\n\t[1] Twitter\n\t[2] Reddit")
+print("\nWelcome to OSINT Toolkit!\n----------------------------------------")
+print("Select the tool you wish to use...\n\n\t[1] Twitter\n\t[2] Reddit\n\n\n")
 
-# Handles user input
-response = [0,1]
-if response in input(1)
-    # execute twitter.py
-if input() = "2"
-    # execute reddit.py
-# all other tools......
-# else print("Please select one of the above options\n----------------------------------------")
-
+userInput = input()
+print(userInput)
 
 
